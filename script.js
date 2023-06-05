c6e3bb2dfc78148ab57eb31bf27b91ac5e2861a0
Kura Adams <kura@missionreadyhq.com>
2023-06-05 19:02:56 +1200

    add .

---
+++ script.js
@@ -0,0 +1,11 @@
+
+for (let index = 1; index <= 10; index++) {
+    console.log(index);
+  }
+
+  const myResString = "Responsiveness";
+  
+  for (let i = 0; i < myResString.length; i++) {
+    console.log(myResString[i]);
+  }
+
